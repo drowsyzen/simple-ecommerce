db22a919493f5083caae6795387f27eeec00d55e
drowsy <away28618@gmail.com>
2024-07-05 01:01:19 +0530

    added JWT auth and some stuff

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simple-ecommerce
+
+
+<!-- - ### add urls for  -->
+ - ## Tasks TODO:    
+    
+    - #### JWT authentication to ensure many users can interact with it.
+    - #### all request to be through JWT auth.
+    - #### Integration with third-party api's (Razorpay, Stripe, Paypal).
+    - #### Ability to register new users.
+    - #### Shopping cart management.
+    - #### Product listings.
+    - #### Ability to create and edit products in the database.
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def example_view(request, format=None):
+    content = {
+        'status': 'request was permitted'
+    }
+    return Response(content)
